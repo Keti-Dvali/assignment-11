5129453bfb5ce49289e97186b5f5995a46410417
Keti-Dvali <104909714+Keti-Dvali@users.noreply.github.com>
2022-09-06 17:22:56 -0700

    finished

---
+++ static/Javascript/script.js
@@ -0,0 +1,10 @@
+let cutomerAtAlpha = {
+    name: "firstname", 
+    lastName: "lastname", 
+    age: 30, 
+    profession: "civil servant", 
+    typeOfInsurance: ["Health Insurance", "Car Insurance", "Travel Insurance"], 
+    insuranceExpirationDate: 2030, 
+}
+
+console.log(cutomerAtAlpha.name, cutomerAtAlpha.lastName, cutomerAtAlpha.age, cutomerAtAlpha.profession, cutomerAtAlpha.typeOfInsurance[0], cutomerAtAlpha.typeOfInsurance[1], cutomerAtAlpha.typeOfInsurance[2], cutomerAtAlpha.insuranceExpirationDate);
